8e5ee5ea6ef44b7fe38ff4a0d6ff76385672cd05
Miquel Peix <mpeix@brujula.es>
2020-09-18 10:23:28 +0200

    Use json utils in getCityWeather

--- src/main/java/training/weather/WeatherForecast.java
+++ src/main/java/training/weather/WeatherForecast.java
@@ -13,6 +13,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import training.weather.clients.RequestClient;
 import training.weather.utils.DateUtils;
+import training.weather.utils.JsonUtils;
 
 public class WeatherForecast {
 
@@ -23,14 +24,12 @@ public class WeatherForecast {
 		if (datetime.before(DateUtils.AddDays(new Date(),7))) {
 			RequestClient client = new RequestClient();
 			JSONArray jsonArray = new JSONArray(client.Get("https://www.metaweather.com/api/location/search/?query=" + city));
-			String cityId = jsonArray.getJSONObject(0).get("woeid").toString();
+			String cityId = JsonUtils.GetJsonValue(jsonArray.getJSONObject(0),"woeid");
 			JSONArray results = new JSONObject(client.Get("https://www.metaweather.com/api/location/" + cityId)).getJSONArray("consolidated_weather");
-			for (int i = 0; i < results.length(); i++) {
-				if (DateUtils.FormatDate("yyyy-MM-dd", datetime)
-					.equals(results.getJSONObject(i).get("applicable_date").toString())) {
-					return results.getJSONObject(i).get("weather_state_name").toString();
-				}
-			}
+
+			String dateFormatted = DateUtils.FormatDate("yyyy-MM-dd", datetime);
+			JSONObject requestedDayWeather = JsonUtils.GetJsonObjectFromKeyValue(results,"applicable_date", dateFormatted);
+			return JsonUtils.GetJsonValue(requestedDayWeather,"weather_state_name" );
 		}
 		return "";
 	}
