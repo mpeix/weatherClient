b3467ab0c22fb9b70393568dca71d0b0b224574c
Miquel Peix <mpeix@brujula.es>
2020-09-18 08:28:33 +0200

    Test Refactor

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -5,14 +5,15 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 public class WeatherForecastTest {
 
 	@Test
-	public void unfinished_test() throws IOException {
+	public void GetCityWeatherTest() throws IOException {
 		WeatherForecast weatherForecast = new WeatherForecast();
 		String forecast = weatherForecast.getCityWeather("Madrid", toDate(LocalDate.now().plusDays(1)));
-		System.out.println(forecast);
+		assertNotNull(forecast);
 	}
 
 	private Date toDate(LocalDate localDate) {
